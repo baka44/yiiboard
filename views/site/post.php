914e18ce856e5b0e93906a37d39f9662d1757429
mars <xmarsxagx@gmail.com>
2020-03-06 17:09:06 +0200

    Separeted template for user information in posts

--- views/site/post.php
+++ views/site/post.php
@@ -2,7 +2,6 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 use app\models\Users;
-use app\models\Posts;
 
 $this->title =  Html::Encode($result['post']['subject']).'|Post';
 $this->params['breadcrumbs'][] = ['label' => $result['category']['name'], 'url' => [ 'site/category', 'categoryId' => $result['category']['id'] ] ];
@@ -26,10 +25,7 @@ $this->params['breadcrumbs'][] = $result['post']['subject'];
   <div class="panel-body">
     <div class="row">
       <div class="col-md-2">
-        <p>user_avatar.jpg</p>
-        <p>USER_RANK</p>
-        <p>Group: <?= Users::findIdentity($result['post']['user_id'])->role ?></p>
-        <p><?= Posts::find()->where(['user_id' => $result['post']['user_id'] ])->count() ?> posts</p>
+        <?= $this->render('blocks/postProfile', ['userId' => $result['post']['user_id'] ]); ?>
       </div>
       <div class="col-md-10">
         <p><?= Html::Encode($result['post']['body']) ?></p>
@@ -56,10 +52,7 @@ $this->params['breadcrumbs'][] = $result['post']['subject'];
     <div class="panel-body">
       <div class="row">
         <div class="col-md-2">
-          <p>user_avatar.jpg</p>
-          <p>USER_RANK</p>
-          <p>Group: <?= Users::findIdentity($reply['user_id'])->role ?></p>
-          <p><?= Posts::find()->where(['user_id' => $reply['user_id'] ])->count() ?> posts</p>
+          <?= $this->render('blocks/postProfile', ['userId' => $reply['user_id'] ]); ?>
         </div>
         <div class="col-md-10">
           <p><?= Html::Encode($reply['body']) ?></p>
